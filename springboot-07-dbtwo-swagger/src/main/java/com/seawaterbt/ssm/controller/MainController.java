acc50a525a12d1c12c67647f01a58c225f513ab4
zsx <1070676593@qq.com>
2020-04-16 16:37:42 +0800

    添加一个双db和swagger的model

---
+++ springboot-07-dbtwo-swagger/src/main/java/com/seawaterbt/ssm/controller/MainController.java
@@ -0,0 +1,14 @@
+package com.seawaterbt.ssm.controller;
+
+import java.sql.SQLOutput;
+
+/**
+ * @CalssName: MainController
+ * @Author: zsx
+ * @Date: 2020/4/15 17:42
+ **/
+public class MainController {
+    public static void main(String[] args) {
+        System.out.println(123);
+    }
+}
---
+++ springboot-07-dbtwo-swagger/src/main/java/com/seawaterbt/ssm/controller/loginController.java
@@ -0,0 +1,23 @@
+package com.seawaterbt.ssm.controller;
+
+/**
+ * @CalssName: loginController
+ * @Author: zsx
+ * @Date: 2020/4/15 17:37
+ **/
+public class loginController {
+    public static void main(String[] args) {
+        System.out.println("分支123");
+        System.out.println("分支已经合并。。。。。");
+        System.out.println("zsx1");
+        System.out.println("zsx1再次提交");
+        System.out.println("在zsx1上创建分支zsx2");
+        System.out.println("创建分支zsx2后再次提交");
+        System.out.println("合并zsx2");
+        System.out.println("分支2合并完成");
+        System.out.println("再创建分支");
+        System.out.println("创建分支2之后再次提交");
+        System.out.println("合并分支1");
+        System.out.println("合并完成");
+    }
+}
