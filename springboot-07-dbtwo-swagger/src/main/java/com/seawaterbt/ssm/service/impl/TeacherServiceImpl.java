ee529bb59b2197a4a999be011414db488de71339
zsx <1070676593@qq.com>
2020-04-24 11:22:37 +0800

    双数据源提交

--- springboot-07-dbtwo-swagger/src/main/java/com/seawaterbt/ssm/service/impl/TeacherServiceImpl.java
+++ springboot-07-dbtwo-swagger/src/main/java/com/seawaterbt/ssm/service/impl/TeacherServiceImpl.java
@@ -16,47 +16,46 @@ import java.util.List;
 import java.util.Map;
 
 @Service
-@DataSource(DataSourceEnum.DB2)
 public class TeacherServiceImpl extends ServiceImpl<TeacherMapper,Teacher> implements TeacherService {
 
     @Override
-    
+    @DataSource(DataSourceEnum.DB2)
     public boolean insert(Teacher entity) {
         return super.insert(entity);
     }
 
     @Override
-    
+    @DataSource(DataSourceEnum.DB2)
     public boolean deleteById(Serializable id) {
         return super.deleteById(id);
     }
 
     @Override
-    
+    @DataSource(DataSourceEnum.DB2)
     public boolean updateById(Teacher entity) {
         return super.updateById(entity);
     }
 
     @Override
-    
+    @DataSource(DataSourceEnum.DB2)
     public Teacher selectById(Serializable id) {
         return super.selectById(id);
     }
 
     @Override
-    
+    @DataSource(DataSourceEnum.DB2)
     public List<Teacher> selectList(Wrapper<Teacher> wrapper) {
         return super.selectList(wrapper);
     }
 
     @Override
-    
+    @DataSource(DataSourceEnum.DB2)
     public Page<Teacher> selectPage(Page<Teacher> page) {
         return super.selectPage(page);
     }
 
     @Override
-    
+    @DataSource(DataSourceEnum.DB2)
     public Page<Teacher> selectPage(Page<Teacher> page, Wrapper<Teacher> wrapper) {
         return super.selectPage(page, wrapper);
     }
